bbbdc9ae163250726d6015f25ec6986c614be6de
anekameni <Kamenialexnea@gmail.com>
2024-12-27 23:44:18 +0100

    Add main function to create llm

---
+++ src/utilities/llm_models.py
@@ -0,0 +1,32 @@
+import os
+from typing import Union
+from enum import Enum
+from langchain_groq import ChatGroq
+from langchain_ollama import ChatOllama
+
+
+class LLMModel(Enum):
+    OLLAMA = ChatOllama
+    GROQ = ChatGroq
+
+
+def get_llm_model(
+        model_type: Union[str, LLMModel], temperature=0, max_tokens=None):
+    if isinstance(model_type, str):
+        model_type = LLMModel[model_type.upper()]
+    if model_type == LLMModel.OLLAMA:
+        return model_type.value(
+            model=os.getenv("OLLAMA_MODEL"),
+            temperature=temperature,
+            max_tokens=max_tokens,
+            # other params...
+            base_url=os.getenv("OLLAMA_HOST"),
+            client_kwargs={
+                "headers": {"Authorization": "Bearer " + os.getenv("OLLAMA_TOKEN")}
+            },
+        )
+    return model_type.value(
+        model=os.getenv("GROQ_MODEL_NAME"),
+        temperature=temperature,
+        max_tokens=max_tokens,
+    )
