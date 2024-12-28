0945d54d4f507e7ab95b4385a0edc0b9c6076d6c
anekameni <Kamenialexnea@gmail.com>
2024-12-28 02:40:46 +0100

    Add simple RAG implementation using langchain and other tools

--- src/utilities/llm_models.py
+++ src/utilities/llm_models.py
@@ -3,6 +3,7 @@ from typing import Union
 from enum import Enum
 from langchain_groq import ChatGroq
 from langchain_ollama import ChatOllama
+from langchain_community.embeddings import OllamaEmbeddings
 
 
 class LLMModel(Enum):
@@ -10,8 +11,9 @@ class LLMModel(Enum):
     GROQ = ChatGroq
 
 
-def get_llm_model(
-        model_type: Union[str, LLMModel], temperature=0, max_tokens=None):
+def get_llm_model_chat(
+    model_type: Union[str, LLMModel], temperature=0, max_tokens=None
+):
     if isinstance(model_type, str):
         model_type = LLMModel[model_type.upper()]
     if model_type == LLMModel.OLLAMA:
@@ -29,4 +31,14 @@ def get_llm_model(
         model=os.getenv("GROQ_MODEL_NAME"),
         temperature=temperature,
         max_tokens=max_tokens,
+    )
+
+
+def get_llm_model_embedding():
+    return OllamaEmbeddings(
+        model=os.getenv("OLLAM_EMB"),
+        base_url=os.getenv("OLLAMA_HOST"),
+        client_kwargs={
+            "headers": {"Authorization": "Bearer " + os.getenv("OLLAMA_TOKEN")}
+        },
     )
