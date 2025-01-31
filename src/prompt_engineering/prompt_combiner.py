8ee56fd970c4ec8e30cb50d94fe909fb399c34bb
Esdras123 <fandioemma@gmail.com>
2025-02-01 00:18:01 +0100

    query with decomposition strategy done

---
+++ src/prompt_engineering/prompt_combiner.py
@@ -0,0 +1,28 @@
+TEMPLATE = """
+Le prompt suivant a été rentré:
+{prompt}
+Vous avez décomposé ce prompt et répondu à chaque sous requête.
+Vous avez récupéré les informations suivantes pour chaque sous-requête :
+{results}
+
+Combinez ces informations pour produire une réponse claire, cohérente et complète au prompt posé en entré.
+"""
+
+
+from langchain.prompts import PromptTemplate
+import json
+
+
+class PromptCombiner:
+    def __init__(self, llm):
+        self.llm = llm
+        self.prompt = PromptTemplate(input_variables=["prompt", "results"], template=TEMPLATE)
+        self.combiner_chain = self.prompt | self.llm
+
+    def __call__(self, prompt, results):
+        for token in self.combiner_chain.stream({"prompt":prompt, "results":results}):
+            print(token.content, end="")
+            # yield token.content
+
+
+    
---
+++ src/prompt_engineering/query_decomposer.py
@@ -0,0 +1,39 @@
+TEMPLATE = """
+Vous êtes un assistant spécialisé dans la reformulation et la décomposition de requêtes utilisateur. Votre tâche consiste à analyser la requête suivante et à effectuer l'une des actions suivantes :
+
+1. Si la requête est complexe (elle contient plusieurs parties ou questions imbriquées), décomposez-la en plusieurs sous-requêtes simples, chacune ciblant un aspect spécifique de la requête.
+2. Si la requête est déjà simple et claire, laissez-la telle quelle, mais placez-la dans un tableau.
+
+Dans tous les cas, fournissez les sous-requêtes ou la requête d'origine sous forme d'un tableau, sans ajouter de commentaires ou d'explications.
+
+### Requête utilisateur :
+{query}
+
+### Résultat attendu (tableau de sous-requêtes ou de la requête unique) :
+[
+"Requête 1",
+"Requête 2",
+...
+]
+"""
+from langchain.prompts import PromptTemplate
+import json
+
+
+class QueryDecomposer:
+    def __init__(self, llm):
+        self.llm = llm
+        self.prompt = PromptTemplate(input_variables=["query"], template=TEMPLATE)
+        self.decomposition_chain = self.prompt | self.llm
+
+    def __call__(self, query):
+        sub_queries = self.decomposition_chain.invoke({"query": query}).content
+
+        try:
+            sub_queries = json.loads(sub_queries)
+            return sub_queries
+        except json.JSONDecodeError:
+            print("output format incorrect, query not divided")
+
+        return [query]
+    
